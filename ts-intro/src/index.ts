af94a4d84391abe06a4f6a8c544f2a41b47c0dc7
Carlos <gohercarlos@gmail.com>
2022-05-08 19:49:31 -0500

    Tarea de oobjectos

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,34 @@
-
-
-console.log('Hola Mundo!!!');
-
 /*
     ===== Código de TypeScript =====
 */
 
+interface Dirr  {
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHeroe {
+    nombre: string;
+    edad: number;
+    direccion: Dirr;
+    mostrarDireccion: () => string;
+
+}
+
+
+const superHeroe: SuperHeroe = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion() {
+        return this.nombre + ', '+this.direccion.ciudad+', '+this.direccion.pais;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
