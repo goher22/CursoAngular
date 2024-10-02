44230b036c46f1032a7c16497dfec3170429c263
Carlos Gomez <gohercarlos@gmail.com>
2024-10-01 19:02:01 -0500

    Lazy Image Part 1

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { CardComponent } from './components/card/card.component';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomePageComponent } from "./pages/home/home-page.component";
+import { SearchBoxComponent } from "./components/search-box/search-box.component";
+import { CardListComponent } from "./components/card-list/card-list.component";
+import { CardComponent } from "./components/card/card.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
-    CardComponent
+    CardComponent,
   ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HomePageComponent,
-  ]
+  imports: [CommonModule, SharedModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "shared-lazy-image",
+  templateUrl: "./lazy-image.component.html",
+  styleUrl: "./lazy-image.component.css",
+})
+export class LazyImageComponent {}
