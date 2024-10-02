6fb91a3666188dc6460c928dc8950c6dc96c8640
Carlos Gomez <gohercarlos@gmail.com>
2024-10-02 15:03:54 -0500

    Funcionamiento del SearchBox

--- 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-by-capital-page',
-  templateUrl: './by-capital-page.component.html',
-  styleUrl: './by-capital-page.component.css'
+  selector: "app-by-capital-page",
+  templateUrl: "./by-capital-page.component.html",
+  styleUrl: "./by-capital-page.component.css",
 })
 export class ByCapitalPageComponent {
-
+  searchByCapital(term: string): void {
+    console.log(term);
+  }
 }
