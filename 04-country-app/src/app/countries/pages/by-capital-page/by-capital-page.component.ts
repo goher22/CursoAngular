2fd136aa27f2baebd60e3fe66e02f0c6142b5498
Carlos Gomez <gohercarlos@gmail.com>
2024-10-10 08:32:00 -0500

    Mostrar y ocultar el LoadingSpinner

--- 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -9,11 +9,14 @@ import { Country } from "../../interfaces/Country";
 })
 export class ByCapitalPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
   constructor(private countriesServices: CountriesService) {}
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.countriesServices.searchCapital(term).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
