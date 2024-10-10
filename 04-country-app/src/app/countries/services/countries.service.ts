fffb810d3d15e7578caf0cee25f7f5f9149cd183
Carlos Gomez <gohercarlos@gmail.com>
2024-10-10 11:35:43 -0500

    LocalStorage del Store

--- 04-country-app/src/app/countries/services/countries.service.ts
+++ 04-country-app/src/app/countries/services/countries.service.ts
@@ -26,7 +26,18 @@ export class CountriesService {
     },
   };
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem("cacheStore", JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage() {
+    if (!localStorage.getItem("cacheStore")) return;
+    this.cacheStore = JSON.parse(localStorage.getItem("cacheStore")!);
+  }
 
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     return this.httpClient.get<Country[]>(`${API_URL}/alpha/${code}`).pipe(
@@ -39,6 +50,7 @@ export class CountriesService {
     return this.httpClient.get<Country[]>(`${API_URL}/capital/${term}`).pipe(
       catchError((error) => of([] as Country[])),
       tap((countries) => (this.cacheStore.byCapital = { term, countries })),
+      tap(() => this.saveToLocalStorage()),
     );
   }
 
@@ -46,6 +58,7 @@ export class CountriesService {
     return this.httpClient.get<Country[]>(`${API_URL}/name/${term}`).pipe(
       catchError((error) => of([] as Country[])),
       tap((countries) => (this.cacheStore.byCountry = { term, countries })),
+      tap(() => this.saveToLocalStorage()),
     );
   }
 
@@ -53,6 +66,7 @@ export class CountriesService {
     return this.httpClient.get<Country[]>(`${API_URL}/region/${region}`).pipe(
       catchError((error) => of([] as Country[])),
       tap((countries) => (this.cacheStore.byRegion = { region, countries })),
+      tap(() => this.saveToLocalStorage()),
     );
   }
 }
