512e82e5f5bae7e2076981705bfe831b47a52357
Carlos Gomez <gohercarlos@gmail.com>
2024-10-18 07:04:09 -0500

    I18nSelectPipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-uncommon-page',
-  templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  selector: "app-uncommon-page",
+  templateUrl: "./uncommon-page.component.html",
+  styleUrl: "./uncommon-page.component.css",
 })
 export class UncommonPageComponent {
+  //i18n Select
+  public name: string = "Carlos";
+  public gender: "male" | "female" = "male";
+  public invitationMap = {
+    male: "invitarlo",
+    female: "invitarla",
+  };
 
+  changeClient(): void {
+    this.name = "Elizabeth";
+    this.gender = "female";
+  }
 }
