e2fa757fb2c39dc193792f0bdf63f071130cbe25
Carlos Gomez <gohercarlos@gmail.com>
2024-10-20 19:14:55 -0500

    Ruta hijas y LazyLoad

--- 06-heroesApp/src/app/auth/auth.module.ts
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from "@angular/common";
 import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { RegisterPageComponent } from "./pages/register-page/register-page.component";
+import { AuthRoutingModule } from "./auth-routing.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +12,6 @@ import { RegisterPageComponent } from "./pages/register-page/register-page.compo
     LoginPageComponent,
     RegisterPageComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
+import { NewPageComponent } from "./pages/new-page/new-page.component";
+
+//localhost:4200/heroes/
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: "new-hero",
+        component: NewPageComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
