f2ce047fc2e0bab26c87909ebd938b35bdbb70dc
Carlos Gomez <gohercarlos@gmail.com>
2024-10-20 21:01:16 -0500

    Path de las rutas faltantes

--- 06-heroesApp/src/app/auth/auth-routing.module.ts
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
+import { RegisterPageComponent } from "./pages/register-page/register-page.component";
 
 //localhost:4200/auth/
 const routes: Routes = [
   {
-    path: "",
+    path: "login",
     component: LayoutPageComponent,
+  },
+  {
+    path: "new-account",
+    component: RegisterPageComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "login",
   },
 ];
 
--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 import { NewPageComponent } from "./pages/new-page/new-page.component";
+import { SearchPageComponent } from "./pages/search-page/search-page.component";
+import { ListPageComponent } from "./pages/list-page/list-page.component";
+import { HeroPageComponent } from "./pages/hero-page/hero-page.component";
 
 //localhost:4200/heroes/
 const routes: Routes = [
@@ -13,6 +16,26 @@ const routes: Routes = [
       {
         path: "new-hero",
         component: NewPageComponent,
+      },
+      {
+        path: "search",
+        component: SearchPageComponent,
+      },
+      {
+        path: "edit/:id",
+        component: NewPageComponent,
+      },
+      {
+        path: "list",
+        component: ListPageComponent,
+      },
+      {
+        path: ":id",
+        component: HeroPageComponent,
+      },
+      {
+        path: "**",
+        redirectTo: "list",
       },
     ],
   },
