bf3680f9e7adc93ed66efc4ed4029bbc4891917a
Carlos Gomez <gohercarlos@gmail.com>
2024-10-23 07:58:57 -0500

    material sidenav, Toolbar e icons

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroPageComponent } from './pages/hero-page/hero-page.component';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { ListPageComponent } from './pages/list-page/list-page.component';
-import { NewPageComponent } from './pages/new-page/new-page.component';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
-
+import { HeroesRoutingModule } from "./heroes-routing.module";
+import { HeroPageComponent } from "./pages/hero-page/hero-page.component";
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
+import { ListPageComponent } from "./pages/list-page/list-page.component";
+import { NewPageComponent } from "./pages/new-page/new-page.component";
+import { SearchPageComponent } from "./pages/search-page/search-page.component";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
   declarations: [
@@ -15,11 +15,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, MaterialModule, HeroesRoutingModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
