929004c4fca4cf89a24f9e42caedaeecb0bc9643
Carlos Gomez <gohercarlos@gmail.com>
2024-10-23 07:12:17 -0500

    Diseño de Login y Auth Layout

--- 06-heroesApp/src/app/auth/auth-routing.module.ts
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 import { RegisterPageComponent } from "./pages/register-page/register-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
 //localhost:4200/auth/
 const routes: Routes = [
   {
-    path: "login",
+    path: "",
     component: LayoutPageComponent,
-  },
-  {
-    path: "new-account",
-    component: RegisterPageComponent,
-  },
-  {
-    path: "**",
-    redirectTo: "login",
+    children: [
+      {
+        path: "login",
+        component: LoginPageComponent,
+      },
+      {
+        path: "new-account",
+        component: RegisterPageComponent,
+      },
+      {
+        path: "**",
+        redirectTo: "login",
+      },
+    ],
   },
 ];
 
