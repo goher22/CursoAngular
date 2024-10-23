6ec87b0adf2fe5cd55620a9534675e8db85efa24
Carlos Gomez <gohercarlos@gmail.com>
2024-10-23 16:47:11 -0500

    HeroesService - Traer informacion de los heroes

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
