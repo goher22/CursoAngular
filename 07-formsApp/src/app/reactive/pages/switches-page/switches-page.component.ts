8510c0857aae667832314899d47a8d119ed2c492
Carlos Gomez <gohercarlos@gmail.com>
2024-10-30 17:29:02 -0500

    COntinuacon de proyecto y tarea

--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  templateUrl: './switches-page.component.html',
-  styles: ``
+  templateUrl: "./switches-page.component.html",
+  styles: ``,
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup = new FormGroup({
+    gender: new FormControl("M", [Validators.required]),
+    wantNotifications: new FormControl(true, [Validators.required]),
+    termsAndConditions: new FormControl(false, [Validators.requiredTrue]),
+  });
 
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
