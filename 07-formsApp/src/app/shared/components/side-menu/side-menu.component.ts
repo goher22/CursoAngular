36a880ca2d42ea88143ac01c00976844f5756c5c
Carlos Gomez <gohercarlos@gmail.com>
2024-10-30 08:49:36 -0500

    Barra de navegacion

---
+++ 07-formsApp/src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: "shared-side-menu",
+  templateUrl: "./side-menu.component.html",
+  styles: ``,
+})
+export class SideMenuComponent {
+  public reactiveMenu: MenuItem[] = [
+    {
+      title: "Basicos",
+      route: "./reactive/basic",
+    },
+    {
+      title: "Dinámios",
+      route: "./reactive/dynamic",
+    },
+    {
+      title: "Switches",
+      route: "./reactive/switches",
+    },
+  ];
+  public authMenu: MenuItem[] = [
+    {
+      title: "Registro",
+      route: "./auth",
+    },
+  ];
+}
