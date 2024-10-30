dde9d656185f5afeddc156ae16ec78461de1b1c7
Carlos Gomez <gohercarlos@gmail.com>
2024-10-30 13:10:30 -0500

    Formularios dinamicos

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  templateUrl: './dynamic-page.component.html',
-  styles: ``
+  templateUrl: "./dynamic-page.component.html",
+  styles: ``,
 })
 export class DynamicPageComponent {
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    favoriteGame: new FormArray([
+      new FormControl("Metal Gear", Validators.required),
+      new FormControl("Death Stranding", Validators.required),
+    ]),
+  });
 
+  get favoriteGames() {
+    return this.myForm.get("favoriteGame") as FormArray;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.reset();
+  }
 }
