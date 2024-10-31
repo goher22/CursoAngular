e0e655a0aebb78357d6e5ad0673c6d961e108c3b
Carlos Gomez <gohercarlos@gmail.com>
2024-10-30 21:38:34 -0500

    Solucion de la tarea

--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,21 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   templateUrl: "./switches-page.component.html",
   styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
   public myForm: FormGroup = new FormGroup({
     gender: new FormControl("M", [Validators.required]),
     wantNotifications: new FormControl(true, [Validators.required]),
     termsAndConditions: new FormControl(false, [Validators.requiredTrue]),
   });
 
+  public person = {
+    gender: "F",
+    wantNotifications: false,
+  };
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  isValidFiedl(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
   onSave(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
   }
 }
