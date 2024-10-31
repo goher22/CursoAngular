7189a0797f1c865f540d3b2b72bf37b0387b99be
Carlos Gomez <gohercarlos@gmail.com>
2024-10-30 23:52:45 -0500

    Formulario de registro

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styles: ``
+  selector: "app-register-page",
+  templateUrl: "./register-page.component.html",
+  styles: ``,
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    userName: new FormControl("", [Validators.required]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    password2: new FormControl("", [Validators.required]),
+  });
 
+  isValidField(field: string) {}
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
 }
