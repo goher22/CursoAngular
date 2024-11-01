96c7348db710ae9c46b7df7b85ecffb63b26b07a
Carlos Gomez <gohercarlos@gmail.com>
2024-10-31 22:09:41 -0500

    Validadores asincronos

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 // import * as customValidators from "../../../shared/validators/calidators";
 import { ValidatorsService } from "../../../shared/services/validators.services";
+import { EmailValidatorService } from "../../../shared/validators.ts/email-validator.service";
 
 @Component({
   selector: "app-register-page",
@@ -11,7 +12,10 @@ import { ValidatorsService } from "../../../shared/services/validators.services"
 export class RegisterPageComponent implements OnInit {
   public myForm!: FormGroup;
 
-  constructor(private validatorsService: ValidatorsService) {}
+  constructor(
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService,
+  ) {}
   ngOnInit(): void {
     this.myForm = new FormGroup({
       name: new FormControl("", [
@@ -19,11 +23,14 @@ export class RegisterPageComponent implements OnInit {
         // Validators.pattern(customValidators.firstNameAndLastnamePattern),
         Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
       ]),
-      email: new FormControl("", [
-        Validators.required,
-        // Validators.pattern(customValidators.emailPattern),
-        Validators.pattern(this.validatorsService.emailPattern),
-      ]),
+      email: new FormControl("", {
+        validators: [
+          Validators.required,
+          // Validators.pattern(customValidators.emailPattern),
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        asyncValidators: [this.emailValidatorService.validate],
+      }),
       userName: new FormControl("", {
         // validators: [Validators.required, customValidators.cantBeStride],
         validators: [Validators.required, this.validatorsService.cantBeStride],
---
+++ 07-formsApp/src/app/shared/validators.ts/email-validator.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from "@angular/forms";
+import { Observable, of } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class EmailValidatorService implements AsyncValidator {
+  validate(
+    control: AbstractControl,
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    return of({
+      emailTaken: true,
+    });
+  }
+}
