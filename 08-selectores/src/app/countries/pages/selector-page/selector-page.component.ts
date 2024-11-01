9a577feae4efbc998c0774613830efcdb902fb42
Carlos Gomez <gohercarlos@gmail.com>
2024-11-01 12:07:22 -0500

    Formulario reactivo - primer selecto

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-selector-page',
-  templateUrl: './selector-page.component.html',
-  styles: ``
+  selector: "app-selector-page",
+  templateUrl: "./selector-page.component.html",
+  styles: ``,
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+  public myForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      region: ["", Validators.required],
+      country: ["", Validators.required],
+      borders: ["", Validators.required],
+    });
+  }
 }
