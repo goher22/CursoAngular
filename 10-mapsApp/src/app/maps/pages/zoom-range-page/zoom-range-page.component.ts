41a47e671f90acfaced3f0143c84eb35d02f8383
Carlos Gomez <gohercarlos@gmail.com>
2024-11-03 13:30:59 -0500

    Controlar el nivel del Zoom

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,13 +9,44 @@ export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild("map")
   public divMap!: ElementRef;
 
+  public curremZoom: number = 10;
+  private map?: Map;
+
   ngAfterViewInit(): void {
     if (!this.divMap) throw "Elemento HTML no fue encontrado";
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement,
       style: "mapbox://styles/mapbox/streets-v12",
       center: [-74.5, 40],
-      zoom: 9,
+      zoom: this.curremZoom,
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) throw "El mapa no se a inicializado";
+
+    this.map.on("zoom", (env) => {
+      this.curremZoom = this.map!.getZoom();
+    });
+
+    this.map.on("zoomend", (env) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChange(value: string) {
+    this.curremZoom = Number(value);
+    this.map?.zoomTo(this.curremZoom);
   }
 }
