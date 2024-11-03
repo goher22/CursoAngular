572ead809632a8e472aa523804b041211bcdc931
Carlos Gomez <gohercarlos@gmail.com>
2024-11-03 13:05:34 -0500

    Diseño - Zoomln y ZoomOut

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import { Map } from "mapbox-gl";
 
 @Component({
-  templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
+  templateUrl: "./zoom-range-page.component.html",
+  styleUrl: "./zoom-range-page.component.css",
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild("map")
+  public divMap!: ElementRef;
 
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw "Elemento HTML no fue encontrado";
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
 }
