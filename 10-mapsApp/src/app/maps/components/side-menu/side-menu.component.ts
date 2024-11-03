702863e9c3ae3f4b1345472731d041c9f7dab5c2
Carlos Gomez <gohercarlos@gmail.com>
2024-11-03 09:43:47 -0500

    Menu de la aplicacion

--- 10-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
+++ 10-mapsApp/src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+interface MenuItem {
+  router: string;
+  name: string;
+}
 
 @Component({
-  selector: 'app-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  selector: "maps-side-menu",
+  templateUrl: "./side-menu.component.html",
+  styleUrl: "./side-menu.component.css",
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { router: "/maps/fullscreen", name: "Full Screen" },
+    { router: "/maps/zoom-range", name: "Zoom Range" },
+    { router: "/maps/markers", name: "Markers" },
+    { router: "/maps/properties", name: "Houses" },
+  ];
 }
