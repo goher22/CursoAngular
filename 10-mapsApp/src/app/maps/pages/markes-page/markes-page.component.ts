18c827609ef9225d2b3f7140c6f666dd3eeeb376
Carlos Gomez <gohercarlos@gmail.com>
2024-11-02 22:05:18 -0500

    Crear los componentes y rutas

---
+++ 10-mapsApp/src/app/maps/pages/markes-page/markes-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "./markes-page.component.html",
+  styleUrl: "./markes-page.component.css",
+})
+export class MarkesPageComponent {}
