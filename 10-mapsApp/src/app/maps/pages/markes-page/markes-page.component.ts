5e36ee9302aa054569d2f836da576297368a9192
Carlos Gomez <gohercarlos@gmail.com>
2024-11-03 17:40:49 -0500

    Marcadores en el mapa

--- 10-mapsApp/src/app/maps/pages/markes-page/markes-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markes-page/markes-page.component.ts
@@ -1,7 +1,32 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import { LngLat, Map, Marker } from "mapbox-gl";
 
 @Component({
   templateUrl: "./markes-page.component.html",
   styleUrl: "./markes-page.component.css",
 })
-export class MarkesPageComponent {}
+export class MarkesPageComponent implements AfterViewInit {
+  @ViewChild("map")
+  public divMap!: ElementRef;
+  private map?: Map;
+  public currentCenter: LngLat = new LngLat(-74.5, 40);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw "Elemento HTML no fue encontrado";
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: this.currentCenter,
+      zoom: 10,
+    });
+
+    // const markerHtml = document.createElement("div");
+    // markerHtml.innerHTML = "Carlos gomez";
+
+    // const marker = new Marker({
+    //   color: "red",
+    //   element: markerHtml,
+    // });
+    // marker.setLngLat(this.currentCenter).addTo(this.map);
+  }
+}
