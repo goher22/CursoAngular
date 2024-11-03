77bdc7fa429dd336b68e14bd08beb90de7bea03a
Carlos Gomez <gohercarlos@gmail.com>
2024-11-03 10:38:26 -0500

    mostrar un mapa en pantalla completa

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,20 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZ29oZXIiLCJhIjoiY2t0N3RxNHIxMHZyeTJ1bXUxcXJvNTRzYiJ9.NVt9uGFBq9bxbPfcTqGJPw";
 
 @Component({
   templateUrl: "./full-screen-page.component.html",
   styleUrl: "./full-screen-page.component.css",
 })
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+}
