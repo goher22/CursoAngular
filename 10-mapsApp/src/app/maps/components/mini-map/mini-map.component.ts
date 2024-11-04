520bbf09e3aca46711b6a23c664e837b4e3f3a03
Carlos Gomez <gohercarlos@gmail.com>
2024-11-04 09:12:36 -0500

    Componente MiniMapa

--- 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
+++ 10-mapsApp/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from "@angular/core";
+import { LngLat, Map, Marker } from "mapbox-gl";
 
 @Component({
-  selector: 'app-mini-map',
-  templateUrl: './mini-map.component.html',
-  styleUrl: './mini-map.component.css'
+  selector: "map-mini-map",
+  templateUrl: "./mini-map.component.html",
+  styleUrl: "./mini-map.component.css",
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @Input()
+  public lngLat?: [number, number];
+  @ViewChild("map")
+  public divMap!: ElementRef;
 
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw "Elemento HTML no fue encontrado";
+    if (!this.lngLat) throw "La latitud y longitud no esta definidad";
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
