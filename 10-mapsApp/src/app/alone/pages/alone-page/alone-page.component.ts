77a5d5b80c458133050c00808581f0a588a041b4
Carlos Gomez <gohercarlos@gmail.com>
2024-11-04 09:48:06 -0500

    Standalone - compo pagina

---
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  templateUrl: "./alone-page.component.html",
+  styleUrl: "./alone-page.component.css",
+})
+export class AlonePageComponent {}
