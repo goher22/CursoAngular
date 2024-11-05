ebe92b3c778f9b3f6b9d6d2f29179ecac32887e0
Carlos Gomez <gohercarlos@gmail.com>
2024-11-05 14:44:05 -0500

    Señales - informacion del usuario

--- 11-directiveSignals/src/app/signals/pages/user-info-pages/user-info-pages.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-pages/user-info-pages.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from "@angular/core";
+import { UserServiceService } from "../../services/user-service.service";
+import { User } from "../../interfaces/user-request.interface";
 
 @Component({
-  templateUrl: './user-info-pages.component.html',
-  styleUrl: './user-info-pages.component.css'
+  templateUrl: "./user-info-pages.component.html",
+  styleUrl: "./user-info-pages.component.css",
 })
-export class UserInfoPagesComponent {
+export class UserInfoPagesComponent implements OnInit {
+  private userService: UserServiceService = inject(UserServiceService);
+  public userId = signal(1);
 
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) return;
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    this.userService
+      .getUserByIs(id)
+      .subscribe((user) => this.currentUser.set(user));
+  }
 }
