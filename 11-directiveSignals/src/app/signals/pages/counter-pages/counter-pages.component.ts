b96f603a388a208b2706b56ea9f9e9f9d77adcbc
Carlos Gomez <gohercarlos@gmail.com>
2024-11-05 13:42:50 -0500

    Counter Signal

--- 11-directiveSignals/src/app/signals/pages/counter-pages/counter-pages.component.ts
+++ 11-directiveSignals/src/app/signals/pages/counter-pages/counter-pages.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: './counter-pages.component.html',
-  styleUrl: './counter-pages.component.css'
+  templateUrl: "./counter-pages.component.html",
+  styleUrl: "./counter-pages.component.css",
 })
 export class CounterPagesComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter());
 
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
 }
