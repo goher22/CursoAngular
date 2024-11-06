366479aee98765d31a1956749e7e3ba0a57f4f59
Carlos Gomez <gohercarlos@gmail.com>
2024-11-06 11:29:18 -0500

    Creaccion de archivos y directorios

---
+++ my-dashboard/src/app/shared/heavy-loaders/user-loaders.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-user-loaders",
+  standalone: true,
+  imports: [],
+  template: ` <h1>Hola mundo</h1> `,
+})
+export class UserLoadersComponent {}
