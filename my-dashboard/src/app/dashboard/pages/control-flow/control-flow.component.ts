4c59f83c663cdfd2460494a2eea329787f85a957
Carlos Gomez <gohercarlos@gmail.com>
2024-11-06 16:41:21 -0500

    control Flow - @Foe

--- my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,6 +11,8 @@ type Grade = "A" | "B" | "C" | "D" | "E" | "F";
 export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>("A");
+  public frameworks = signal(["Angular", "Vuejs", "Svelte", "Qwik", "React"]);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update((value) => !value);
