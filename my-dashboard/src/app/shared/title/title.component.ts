98d99ac4041954b8d3ed8f5d860a51bd52d00acc
Carlos Gomez <gohercarlos@gmail.com>
2024-11-06 16:53:55 -0500

    Required @Input

--- my-dashboard/src/app/shared/title/title.component.ts
+++ my-dashboard/src/app/shared/title/title.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { booleanAttribute, Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-title",
   standalone: true,
   imports: [],
-  templateUrl: "./title.component.html",
-  styles: ``,
+  template: `<h1 class="text-3xl mb-5">{{ title }}</h1>`,
 })
-export class TitleComponent {}
+export class TitleComponent {
+  @Input({ required: true })
+  public title!: string;
+
+  @Input({ transform: booleanAttribute })
+  public withShadow: boolean = false;
+}
